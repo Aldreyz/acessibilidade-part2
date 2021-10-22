d6a756ce116dcc4174e7e3f3e8888a46d6aecf3e
Aldrey <aldreybp@gmail.com>
2021-10-22 16:02:55 -0300

    Injetando templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { TemplateRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
+  public firstName = 'Aldrey';
+  public modalRef :ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  public show(): void {
+    this.modalRef=this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
 }
