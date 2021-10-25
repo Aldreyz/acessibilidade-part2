4c59aa10b95375c6551828a4e6d1fe14659c899f
Aldrey <aldreybp@gmail.com>
2021-10-25 14:21:46 -0300

    FadeIn

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,10 +1,13 @@
+import { fade } from './../../animations/fade';
 import { AfterViewInit, Component, HostBinding } from "@angular/core";
 import { ModalConfig } from "./interfaces/modal-config";
 @Component({
   selector:'app-modal',
   templateUrl:'./modal.component.html',
-  styleUrls:['modal.component.scss']
+  styleUrls:['modal.component.scss'],
+  animations:[fade]
 })
 export class ModalComponent {
+  @HostBinding('@fade') fade=true
   public config:ModalConfig;
 }
